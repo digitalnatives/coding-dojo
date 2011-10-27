70061726cf12b246b472d0fb8809138c88e5a053
Tamas Tompa <tompata@gmail.com>
2011-10-27 09:55:39 +0200

    New english numerals rails project

---
+++ numerals/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Numerals::Application.config.secret_token = '39bf05b0e11f1e32749790ef3378cb0aeccde4600081c09f3569e2398a6b0f2786688cd1f869c9ea0efbca024ed3c092f77a8ea66619afde0225aa64c105129e'
