621e3c11371a3e5d83b2a78e320660673173dea3
Istvan DEMETER <istvan.demeter@digitalnatives.hu>
2012-09-14 03:44:38 +0200

    my first solution

--- number_ocr/ta-di-bm/reader.rb
+++ number_ocr/ta-di-bm/reader.rb
@@ -11,8 +11,7 @@ mapping = {
   ' . ... ..' => 9,
 }
 
-
-seq = [[]]
+seq, numbers = [[]], []
 File.open('input.txt', 'r').each_line do |line|
   if line =~ /^$/
     seq << []
@@ -21,27 +20,9 @@ File.open('input.txt', 'r').each_line do |line|
   seq.last << line.chomp.gsub!(/[^\s]/, '.')
 end
 
-numbers = []
 seq.each do |s|
   numbers << []
   (0..24).step(3) { |i| numbers.last << mapping[s.map { |j| j[i..(i+2)] }.join] }
   numbers[-1] = numbers.last.join.to_i
 end
-
 puts numbers
-
-@seq = seq
-@numbers = numbers
-#   seq[i] << line[0..-2].split('').map{|c| c == ' ' ? 0 : 1 }
-#   seq[i].last << 0 if seq[i].last.count == 26
-# end
-
-# seq_digits = []
-# seq.each do |line|
-#   seq_digits << []
-#   (0..2).each do |i|
-#     while l = line[i].shift(3)
-#       seq_digits.last << l
-#     end
-#   end
-# end
