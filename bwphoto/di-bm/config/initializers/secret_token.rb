e8752086b80f1390790dac002f9c7c4e09f40ea9
whatwho <beothy.miklos@gmail.com>
2012-09-18 10:07:07 +0200

    initial commit, rails and test environment

---
+++ bwphoto/di-bm/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiBm::Application.config.secret_token = '3ab3c80a58b8e82ba23fdb9b2364a7b06f9bc1567ef57e5f95ff0119ba16728c8b1d35c39fcb10364f04f907b059da5cc265f821bbb33e7b3a97cbbebdb20f34'
