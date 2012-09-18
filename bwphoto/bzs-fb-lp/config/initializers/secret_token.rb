26db53b123c465370d84a8e12d674f23b97fd49c
Bfleischmann <boni.digitalnatives@gmail.com>
2012-09-18 10:07:09 +0200

    initial commit

---
+++ bwphoto/bzs-fb-lp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BzsFbLp::Application.config.secret_token = '0c1de44ec9a72e5c221bc05284891dc6134ae68b51087d1bedd9983c7b8df30541f2b537aa24227adcf502dda6d007d6f82ce40d8f44d9f218d376a633cb212b'
