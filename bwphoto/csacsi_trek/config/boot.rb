7dca745cbc693b0d609986af0fd6f973c78c3295
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2012-10-09 09:42:28 +0200

    Add db specific rake tasks

--- bwphoto/csacsi_trek/config/boot.rb
+++ bwphoto/csacsi_trek/config/boot.rb
@@ -5,6 +5,7 @@ Bundler.setup
 
 Bundler.require(:default, ENV["RACK_ENV"].to_sym)
 $redis = Redis.connect
+DataMapper.setup(:default, ENV["DATABASE_URL"] || "postgres://postgres:postgres@localhost/bwphoto_development" )
 
 Dir["./lib/**/*.rb"].each { |f| require f }
 
