09859ff18e75d3c381e962023f245a7f07f6c230
Szikszai Gusztáv <guszti5@hotmail.com>
2012-10-09 10:14:01 +0200

    implementation

--- bwphoto/ta-szg-bg/app.rb
+++ bwphoto/ta-szg-bg/app.rb
@@ -4,15 +4,31 @@ Bundler.require(:default)
 
 require './db'
 
-class RackApp < Sinatra::Application
-  get "/image" do
-  end
-  put "/image" do
-    img = Picture.new(request.params)
-    if img.save
-      {status: 200, errors: []}.to_json
-    else
-      {status: 500, errors: img.errors}.to_json
+class RackApp < Grape::API
+  format :json
+
+  resource :image do
+    get do
+      {test: 'asd'}
+    end
+    post do
+      img = Picture.new({
+        id: params[:id],
+        title: params[:title],
+        camera: params[:camera],
+        date: params[:date],
+        author: params[:author],
+        picture: params[:picture],
+        filename: params[:filename]
+      })
+      if img.save
+        Worker.perform_async img.id
+        {status: 200, errors: []}
+      else
+        errors = []
+        img.errors.map {|error| errors.push error[0]}
+        {status: 500, errors: errors}
+      end
     end
   end
 end
