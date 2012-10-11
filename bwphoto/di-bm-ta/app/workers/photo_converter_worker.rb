5986bcabd6e65e3008d55c2fe789e1bbbf479f4c
Istvan DEMETER <istvan.demeter@digitalnatives.hu>
2012-10-11 10:10:08 +0200

    csutortok

--- bwphoto/di-bm-ta/app/workers/photo_converter_worker.rb
+++ bwphoto/di-bm-ta/app/workers/photo_converter_worker.rb
@@ -1,13 +1,16 @@
+require 'open-uri'
+
 class PhotoConverterWorker
   include Sidekiq::Worker
 
   def perform(photo_id)
-    download(photo_id)
+    @photo = Photo.find(photo_id)
+    download(photo_id) if @photo.url.present?
     convert(photo_id)
   end
 
   def download(photo_id)
-
+    @photo.photo = open(@photo.url).read
   end
 
   def convert(photo_id)
