0367accb3450d46a9019b56d2b9573f3dede96ec
Bfleischmann <boni.digitalnatives@gmail.com>
2012-11-09 10:18:18 +0100

    validation organized, image processing moved to model

--- bwphoto/fb_sg/spec/spec_helper.rb
+++ bwphoto/fb_sg/spec/spec_helper.rb
@@ -41,4 +41,25 @@ RSpec.configure do |config|
     # http://www.coffeepowered.net/2011/07/15/resque-and-tests
     Resque.redis.select 1
   end
+
+  config.after(:all) do
+    if Rails.env.test? 
+      FileUtils.rm_rf(Dir["#{Rails.root}/spec/support/uploads"])
+    end 
+  end
+end
+
+if defined?(CarrierWave)
+  CarrierWave::Uploader::Base.descendants.each do |klass|
+    next if klass.anonymous?
+    klass.class_eval do
+      def cache_dir
+        "#{Rails.root}/spec/support/uploads/tmp"
+      end 
+
+      def store_dir
+        "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+      end 
+    end 
+  end 
 end
